6f04f9088f8544a3c57e85f30d536b16bb224804
JOURT1 <jhoelsuarez02@gmail.com>
2025-03-30 23:57:22 -0500

    Personaje con animaciones y configuracion tercera persona

---
+++ Assets/scripts/PLayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer : MonoBehaviour
+{
+    public float runspeed = 5;
+    public float rotationSpeed = 250;
+
+    public Animator animator;
+
+    private float x, y;
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * runspeed);
+
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+
+    }
+}
